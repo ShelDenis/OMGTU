67c8a23716fee053fc8eb907fd9e4aa60f97f91d
ShelDenis <144816515+ShelDenis@users.noreply.github.com>
2023-10-10 14:42:10 +0600

    Add Krestianin i chert

---
+++ Olimpic_tasks/peasant_and_devil_v2.py
@@ -0,0 +1,9 @@
+n_max = int(input())
+count = 0
+# print('n k z')
+for n in range(1, n_max + 1):
+    for z in range(2, 18):
+        if n % (2 ** z - 1) == 0:
+            count += 1
+            
+print(count + n_max)
